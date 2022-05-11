ab85b8736a24a65650649aa881ac1d86e8dd85ca
LFLATH <ludwiglechtreck@gmail.com>
2022-05-11 12:41:00 -0400

    Semi-Working 1-n particle collision simulation. Problems include, tunneling, optimization, and collisions not being based on mass yet.

--- particle.py
+++ particle.py
@@ -1,18 +1,40 @@
 import pygame
+import math
 class Particle():
-    def __init__(self, mass, cords, velocity, screen):
+    def __init__(self, mass, cords, velocity, acceleration, screen):
         self.mass = mass
         self.cords = cords
         self.screen = screen
         self.velocity = velocity
+        self.acceleration = acceleration
+    def get_pos(self):
+        return (self.cords[0], self.cords[1], self.mass)
     def spawn(self):
         pygame.draw.circle(self.screen,(255,255,255),(self.cords[0], self.cords[1]), self.mass)
     def move(self):
-        self.cords[0] +=  self.velocity[0]
-        self.cords[1] += self.velocity[1]
+        self.velocity[0] = self.velocity[0] + self.acceleration[0] * (0.16666666)#0.166 is 1/60 or dt
+        self.velocity[1] = self.velocity[1] + self.acceleration[1] * (0.16666666)
+        self.cords[0] = self.cords[0] + self.velocity[0] * 0.16666666
+        self.cords[1] = self.cords[1] +  self.velocity[1] * 0.16666666
+        self.hit_edge()
     def hit_edge(self):
         boundx, boundy = self.screen.get_size()
-        if self.cords[0] >= boundx - 10 or self.cords[0] <= self.mass:
+        if self.cords[0] + self.mass >= boundx or self.cords[0] - self.mass <= 0:
             self.velocity[0] = self.velocity[0] * -1
-        if self.cords[1] >= boundy - 10 or self.cords[1] <= self.mass:
+        if self.cords[1] + self.mass >= boundy or self.cords[1] - self.mass <= 5:
             self.velocity[1] = self.velocity[1] * -1
+
+
+    def collide(self, particles):
+        for particle in particles:
+            particle = particle.get_pos()
+            if  [int(particle[0]), int(particle[1])] == [int(self.cords[0]), int(self.cords[1])]:
+                continue
+        
+            elif math.dist([particle[0], particle[1]], [self.cords[0], self.cords[1]]) <= self.mass + particle[2]:
+                print(math.dist([particle[0], particle[1]], [self.cords[0], self.cords[1]]))
+                self.velocity[1] = self.velocity[1] * -1
+                self.velocity[0] = self.velocity[0] * -1
+                
+               
+
--- simulation.py
+++ simulation.py
@@ -8,12 +8,11 @@ clock = pygame.time.Clock()
 black = [0,0,0]
 screen = pygame.display.set_mode((500,500))
 particles = []
-for i in range(0,11):
-    particle = Particle(10,[0 + random.randint(0, 400), 0 + random.randint(0, 400)],[random.randint(1,2), random.randint(-1, 1)], screen)
+for i in range(0,4):
+    particle = Particle(20,[0 + random.randint(40, 400), 0 + random.randint(40, 400)],[random.randint(10,11), random.randint(-5, 5)],[0,0], screen)
     particles.append(particle)
 
 while True:
-    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -21,7 +20,10 @@ while True:
     
     screen.fill(black)
     for particle in particles:
-        particle.hit_edge()
         particle.move()
+        particle.collide(particles)
         particle.spawn()
+
+    clock.tick(120)
+
     pygame.display.update()
